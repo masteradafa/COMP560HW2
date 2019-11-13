7bfedf1aa51c34f6ca07a4eb50b2ff573b152189
B D <dongbowei0526@gmail.com>
2019-11-13 14:03:33 -0500

    Update Train.py

--- Train.py
+++ Train.py
@@ -9,6 +9,6 @@ for i in range(1,4):
 game = TicTacToe(True) #game instance, True means training
 player1= Qlearning() #player1 learning agent
 player2 =Qlearning() #player2 learning agent
-game.startTraining(player1,player2) #start training
-game.train(ites[2],ites) #train for 200,000 iterations
-game.saveStates()  #save Qtable
+game.startTraining(player1,player2)
+game.train(ites[2],ites)
+game.saveStates() 
